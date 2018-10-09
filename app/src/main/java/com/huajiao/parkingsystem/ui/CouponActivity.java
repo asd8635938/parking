0115bc32d11b866515286a6516f15a726630632b
xiong <xiongzhongyu@parteam.cn>
2018-10-09 11:47:12 +0800

    ""

--- app/src/main/java/com/huajiao/parkingsystem/ui/CouponActivity.java
+++ app/src/main/java/com/huajiao/parkingsystem/ui/CouponActivity.java
@@ -1,6 +1,8 @@
 package com.huajiao.parkingsystem.ui;
 
 
+import android.util.TypedValue;
+
 import com.huajiao.parkingsystem.R;
 import com.huajiao.parkingsystem.base.BaseActivity;
 
@@ -31,5 +33,16 @@ public class CouponActivity extends BaseActivity {
     @Override
     protected void getInternetData() {
 
+    }
+
+    /**
+     * 根据dip值转化成px值
+     *
+     * @param dip
+     * @return
+     */
+    private int dipToPix(float dip) {
+        int size = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dip, getResources().getDisplayMetrics());
+        return size;
     }
 }
